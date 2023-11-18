00d70ae13bd02297c667a3e412e7f7a1be745fef
jubarex <juliano@notari.com.br>
2023-11-18 16:56:00 -0300

    produtos restful

--- main-server-2.js
+++ main-server-2.js
@@ -328,68 +328,98 @@ app.delete('/lojas/:lojaID', verifyJWT, (req, res) => {
 
 // CREATE TABLE Produtos
 // !PRODUTO
-// inserir produto 
-app.post('/produtos', verifyJWT, (req, res) => {
-  const { nome, descricao, preco, precoPromocional, idLoja } = req.body;
+// Inserir produto
+app.post('/produtos', verifyJWT, async (req, res) => {
+  try {
+    const { nome, descricao, preco, precoPromocional, idLoja } = req.body;
 
-  const query = 'INSERT INTO Produtos (nome, descricao, preco, precoPromocional, idLoja) VALUES (?, ?, ?, ?, ?)';
-  banco.conn.query(query, [nome, descricao, preco, precoPromocional, idLoja], (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ error: 'Erro ao cadastrar produto.' });
-      return;
+    // Validar entrada
+    if (!nome || !descricao || !preco || !idLoja) {
+      return res.status(400).json({ error: 'Dados incompletos para cadastrar o produto.' });
     }
 
-    res.status(201).json({ message: 'Produto cadastrado com sucesso.', product: { id: results.insertId, nome, descricao, preco, precoPromocional, idLoja } });
-  });
+    const query = 'INSERT INTO Produtos (nome, descricao, preco, precoPromocional, idLoja) VALUES (?, ?, ?, ?, ?)';
+    const results = await banco.conn.query(query, [nome, descricao, preco, precoPromocional, idLoja]);
+
+    res.status(201).json({ message: 'Produto cadastrado com sucesso.', data: { id: results.insertId, nome, descricao, preco, precoPromocional, idLoja } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao cadastrar produto.' });
+  }
 });
 
-// listar produtos 
-app.get('/produtos', (req, res) => {
-  const query = 'SELECT * FROM Produtos';
-  banco.conn.query(query, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ error: 'Erro ao listar produtos.' });
-      return;
-    }
+// Listar produtos
+app.get('/produtos', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM Produtos';
+    const results = await banco.conn.query(query);
 
-    res.status(200).json({ products: results });
-  });
+    res.status(200).json({ data: { products: results } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao listar produtos.' });
+  }
 });
 
-// Atualizar um produto pelo pelo ID 
-app.put('/produtos/:produtoID', verifyJWT, (req, res) => {
-  const produtoID = req.params.produtoID;
-  const { nome, descricao, preco, precoPromocional } = req.body;
+// Obter um produto pelo ID
+app.get('/produtos/:produtoID', async (req, res) => {
+  try {
+    const produtoID = req.params.produtoID;
 
-  const query = 'UPDATE Produtos SET nome = ?, descricao = ?, preco = ?, precoPromocional = ? WHERE idProduto = ?';
-  banco.conn.query(query, [nome, descricao, preco, precoPromocional, produtoID], (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ error: 'Erro ao atualizar o produto.' });
-      return;
+    const query = 'SELECT * FROM Produtos WHERE idProduto = ?';
+    const results = await banco.conn.query(query, [produtoID]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'Produto não encontrado.' });
     }
+
+    res.status(200).json({ data: { product: results[0] } });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao obter o produto.' });
+  }
+});
+
+// Atualizar um produto pelo ID
+app.put('/produtos/:produtoID', verifyJWT, async (req, res) => {
+  try {
+    const produtoID = req.params.produtoID;
+    const { nome, descricao, preco, precoPromocional } = req.body;
+
+    // Validar entrada
+    if (!nome || !descricao || !preco) {
+      return res.status(400).json({ error: 'Dados incompletos para atualizar o produto.' });
+    }
+
+    const query = 'UPDATE Produtos SET nome = ?, descricao = ?, preco = ?, precoPromocional = ? WHERE idProduto = ?';
+    await banco.conn.query(query, [nome, descricao, preco, precoPromocional, produtoID]);
 
     res.status(200).json({ message: 'Produto atualizado com sucesso.' });
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao atualizar o produto.' });
+  }
 });
 
 // Deletar um produto pelo ID
-app.delete('/produtos/:produtoID', verifyJWT, (req, res) => {
-  const produtoID = req.params.produtoID;
+app.delete('/produtos/:produtoID', verifyJWT, async (req, res) => {
+  try {
+    const produtoID = req.params.produtoID;
 
-  const query = 'DELETE FROM Produtos WHERE idProduto = ?';
-  banco.conn.query(query, [produtoID], (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ error: 'Erro ao deletar o produto.' });
-      return;
+    const query = 'DELETE FROM Produtos WHERE idProduto = ?';
+    const results = await banco.conn.query(query, [produtoID]);
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ error: 'Produto não encontrado.' });
     }
 
     res.status(200).json({ message: 'Produto deletado com sucesso.' });
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao deletar o produto.' });
+  }
 });
+
 
 //?lojaID=1&marcaID=1&categoriaID=1&atributoID=1&valorAtributoID=1 
 app.get('/buscarProdutos', verifyJWT, (req, res) => {
